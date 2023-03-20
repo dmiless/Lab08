7d8b08991f5290e762c6491377636475b5b74dde
dmiless <dylanhmiles@gmail.com>
2023-03-20 10:37:33 -0400

    helloWorld user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,6 @@
-print("Hello World, my name is Dylan and I love Python!!")
+
+userName = input ("Hello! Please enter your name:")
+
+print("Hello", userName, "nice to meet you")
+
+#print("Hello World, my name is Dylan and I love Python!!")
